27e42f6f724d387df6fb62cabb7f7376771e9336
Jett <jettc505@gmail.com>
2022-09-30 13:39:25 -0600

    Added funtions

--- Assets/script.js
+++ Assets/script.js
@@ -11,13 +11,18 @@ function random_num_gen() {
   return randInt;
 }
 
-function random_let_gen() {
+// capitol and lowercase letters
+var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function random_let_gen(alpa_string) {
   // Generate random character from the english alphabet
-  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var alpha = alpa_string;
   var randCharNum = Math.floor(Math.random() * alpha.length);
   var randChar = alpha[randCharNum];
   return randChar;
 }
+
 
 function random_char_gen() {
   // Generate random character from the english alphabet
@@ -32,30 +37,42 @@ function random_char_gen() {
 
 function check_criteria() {
   // Checks the criteria given
-  var get_length = prompt("How many characters do you want");
-  console.log(get_length);
-  if (get_length < 8 || get_length > 128) {
-    alert("Password must be at least 8 characters long");
-    var get_length = prompt("How many characters do you want");
-  } else {
-    var get_numOfNums = prompt("How many are numbers");
-    var get_specialChar = prompt("How many are special characters");
+  var length = getLength();
+
+  function getLength() {
+    // Gets the length of the password
+    while (true) {
+      var get_length = prompt("How many characters do you want");
+      console.log(get_length);
+      if (get_length < 8 || get_length > 128) {
+        alert("Password must be at least 8 characters long");
+      } else {
+        break;
+      }
+    }
+    return get_length;
   }
 
-
+  function getCase() {}
+  function getSchar() {}
 
 }
 
+function generatePassword(length) {
+  // Generates the password from the criteria
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password + random_let_gen(lowercase_letters);
+  }
+  return password;
+}
 // Write password to the #password input
 function writePassword() {
   password = generatePassword();
 }
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  // Gets the criteria for the password 
-  check_criteria();
-  var get_specialChar = prompt("How many are special characters");
+  var password = generatePassword(check_criteria.get_length());
 
   var passwordText = document.querySelector("#password");
 
@@ -66,5 +83,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// TODO: Make the password generator 
+// TODO: Make the password generator
 // TODO: Make the get_criteria function not go on after a mistake has bin made twice
+// TODO: FInd out what to do with capitol letters
